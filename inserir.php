eb8757120ffb9aa80e937043bf35e08428b0058e
name_do_github <email_github@gmail.com>
2022-09-29 16:07:28 -0300

    login validation

--- inserir.php
+++ inserir.php
@@ -4,9 +4,11 @@
     $email = $_POST["email"];
     $senha = MD5($_POST["senha"]);
 
-    $comando = $pdo -> prepare("INSERT INTO usuario(email,senha) VALUES(:email,:senha)");  
+    $comando = $pdo -> prepare("INSERT INTO usuario(email_usuario,senha_usuario, is_adm_usuario) VALUES(:email,:senha,0)");  
     $comando->bindValue(":email",$email);                                     
     $comando->bindValue(":senha",$senha);                                  
-                               
-    header("Location:index.html");
+    
+    $comando->execute();
+
+    header("Location:cadastrar.php");
 ?>
