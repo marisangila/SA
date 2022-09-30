57603c14723436495dc2183e49e7c65313f8a762
marisangila <marisangila.alves@edu.sc.senai.br>
2022-09-30 12:20:47 -0300

    update

--- database.sql
+++ database.sql
@@ -1,68 +1,31 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
+CREATE TABLE `usuario` (
+  `pk_usuario` int(11) NOT NULL,
+  `email_usuario` varchar(100) NOT NULL,
+  `senha_usuario` varchar(100) NOT NULL,
+  `is_adm_usuario` bit(1) NOT NULL DEFAULT b'0'
+) 
 --
--- Host: 127.0.0.1
--- Tempo de geração: 29-Set-2022 às 21:48
--- Versão do servidor: 10.4.24-MariaDB
--- versão do PHP: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+-- Criar banco de banco de dados chamado: `php`
 --
--- Banco de dados: `php`
---
-
 -- --------------------------------------------------------
 
 --
 -- Estrutura da tabela `usuario`
 --
-
 CREATE TABLE `usuario` (
   `pk_usuario` int(11) NOT NULL,
   `email_usuario` varchar(100) NOT NULL,
   `senha_usuario` varchar(100) NOT NULL,
   `is_adm_usuario` bit(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Extraindo dados da tabela `usuario`
---
-
-INSERT INTO `usuario` (`pk_usuario`, `email_usuario`, `senha_usuario`, `is_adm_usuario`) VALUES
-(1, 'teste@teste', '202cb962ac59075b964b07152d234b70', b'0'),
-(6, 'adm@adm', '202cb962ac59075b964b07152d234b70', b'1');
-
---
--- Índices para tabelas despejadas
---
-
 --
 -- Índices para tabela `usuario`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`pk_usuario`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `pk_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `pk_usuario` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- inserir.php
+++ inserir.php
@@ -3,13 +3,12 @@
 
     //atribuindo valores dos campos a variaveis.
     $email = $_POST["email"];
-    $email = $_POST["email"];
 
     //converte a senha para hash MD5, para que não seja armazenada em texto limpo no banco de dados.
     $senha = MD5($_POST["senha"]);
 
     //comando SQL.
-    $comando = $pdo -> prepare("INSERT INTO usuario(email_usuario,senha_usuario, is_adm_usuario) VALUES(:email,:senha,0)");  
+    $comando = $pdo -> prepare("INSERT INTO usuario(email_usuario,senha_usuario) VALUES(:email,:senha)");  
     
     //insere valores das variaveis no comando sql.
     $comando->bindValue(":email",$email);                                     
