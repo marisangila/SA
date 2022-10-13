21bcae644e6fd867456a67cf6da66b16a8f7142a
marischatten <marischatten@gmail.com>
2022-10-13 14:57:30 -0300

    edit

--- informacoes_usuario.php
+++ informacoes_usuario.php
@@ -18,11 +18,16 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 </head>
 
 <body>
+    <?php
+        include("listar_usuario_conectado.php");
+        echo "<p>Bem vindo ".$informacoes_usuario['nome_usuario']."!</p>";
+    ?>
     <h3>Listar Usuários:</h3>
     <table border="1">
         <thead>
             <tr>
                 <th>Foto</th>
+                <th>Nome</th>
                 <th>Código</th>
                 <th>Email</th>
                 <th>Nível de Acesso</th>
@@ -30,17 +35,14 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
         </thead>
         <tbody>
             <?php
-            include("listar_usuario_conectado.php");
             include("listar_usuarios.php");
 
-            echo "<p>Bem vindo ".$informacoes_usuario['nome_usuario']."!</p>";
-
             //verifica se a variável tem os valores da tabela.
-            if (!empty($resultado)) {
+            if (!empty($lista_usuarios)) {
                 //seleciona linha por linha.
-                foreach ($resultado as $linha) { ?>
+                foreach ($lista_usuarios as $linha) { ?>
                     <tr>
-                        <td> <?php echo '<img height="40px" width="40px" src="' .$linha['imagem_usuario']. '">'; ?>
+                        <td> <?php echo '<img height="40px" width="40px" src="' .$linha['imagem_usuario']. '">'; ?> </td>
                         <td> <?php echo $linha['pk_usuario']; ?></td>
                         <td> <?php echo $linha['nome_usuario']; ?></td>
                         <td> <?php echo $linha['email_usuario']; ?></td>
